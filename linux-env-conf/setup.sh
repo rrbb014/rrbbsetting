fb228cc548a05b9e3ac978eb079bbb99263401e5
rrbb014 <gutssoul1@gmail.com>
2019-06-21 14:17:12 +0900

    update vim plugin

--- linux-env-conf/setup.sh
+++ linux-env-conf/setup.sh
@@ -3,6 +3,9 @@ INFO_PREFIX="\e[1m\e[32mINFO\e[0m:"
 
 EXEC_PATH=$(readlink -f $(dirname "$0"))
 
+# GIT
+sudo apt-get install -y git
+
 # TMUX
 sudo apt-get remove -y tmux
 sudo apt-get update
@@ -21,6 +24,16 @@ echo -e $INFO_PREFIX "Moved $EXEC_PATH/tmux.conf ==> ~/.tmux.conf"
 tmux source ~/.tmux.conf
 
 # VIM
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install Exuberant-ctags for Tagbar plugin
+sudo apt-get install exuberant-ctags
+
+# Install CScope
+sudo apt-get install cscope
+
 # Move Vim config
 echo -e source $EXEC_PATH/vimrc > ~/.vimrc
 echo -e $INFO_PREFIX "Moved $EXEC_PATH/vimrc ==> ~/.vimrc"
