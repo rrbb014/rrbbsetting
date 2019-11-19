e8b75c414fd7a6d59d96d52fd830468e4b105b65
rrbb014 <gutssoul1@gmail.com>
2019-11-19 17:53:27 +0900

    For pydocker testing setting all values script

---
+++ scripts/linux/set_values_for_testing.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./zhk zk set /gauss/conf/hdfs/url=hdfs:50070
+./zhk zk set /gauss/conf/ml/pred/api/backend=new
+./zhk zk set /gauss/conf/ml/train/api/backend=new
+./zhk zk set /gauss/conf/kafka/bootstrap/servers=broker:9092
