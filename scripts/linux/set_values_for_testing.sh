d35e06fbc1f8b81386e53b4b64ff8c3d9a3504c3
rrbb014 <gutssoul1@gmail.com>
2019-12-02 15:16:13 +0900

    ADDED: feedback directory in HDFS for feedback service

--- scripts/linux/set_values_for_testing.sh
+++ scripts/linux/set_values_for_testing.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Zookeeper
+
 ./zhk zk set /gauss/conf/hdfs/url=hdfs:50070
 ./zhk zk set /gauss/conf/ml/pred/api/backend=new
 ./zhk zk set /gauss/conf/ml/train/api/backend=new
 ./zhk zk set /gauss/conf/kafka/bootstrap/servers=broker:9092
+
+# HDFS
+sudo ./zhk hdfs mkdir /feedback
